a90c48646c487e6995f62837dd39af976bd0758f
Sait Ahmet <62621911+yetgins@users.noreply.github.com>
2021-06-04 11:02:50 +0300

    Create socket.io server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const PORT = 3000;
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
+
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} diconnected`);
+    socket.leave(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
