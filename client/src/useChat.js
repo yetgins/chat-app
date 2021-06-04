1b4b8ba032ef2fbec98de52dbcf8dc14ef159086
Sait Ahmet <62621911+yetgins@users.noreply.github.com>
2021-06-04 16:17:36 +0300

    final changes

--- client/src/useChat.js
+++ client/src/useChat.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-const SOCKET_SERVER_URL = "http://localhost:3000";
+const SOCKET_SERVER_URL = "http://localhost:4000";
 
 const useChat = (roomId) => {
   const [messages, setMessages] = useState([]);
